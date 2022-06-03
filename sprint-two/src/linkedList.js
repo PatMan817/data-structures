1552e23b711ccad5af2077282fb709ba8ed09eb9
Patrick O'Shea <osheap81796@gmail.com>
2022-06-03 14:17:39 -0400

    Completed linked list and tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,53 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newTail = Node(value);
+    if (list.tail === null) {
+      list.tail = newTail;
+    } else {
+      list.tail.next = newTail;
+      list.tail = newTail;
+    }
+    if (list.head === null) {
+      list.head = newTail;
+    }
   };
 
   list.removeHead = function() {
+    //if head is null, return
+    if (list.head === null) {
+      return;
+    }
+    //if head.next is null make head null
+    if (list.head.next === null) {
+      var oldHead = list.head;
+      list.head = null;
+      return oldHead.value;
+    } else {
+      //point list.head to list.head.next
+      var oldHead = list.head;
+      list.head = list.head.next;
+      return oldHead.value;
+    }
   };
 
   list.contains = function(target) {
+    //base case
+    var innerNodeCheck = function(currentNode) {
+      if (currentNode.value === target) {
+        return true;
+      }
+      //recursive case
+      //if currentnode.next !== null
+      if (currentNode.next !== null) {
+        return innerNodeCheck(currentNode.next);
+      }
+      return false;
+    };
+    return innerNodeCheck(list.head);
+    //call contains on list.contains.apply(currentnode.next, target)
+    //return false
   };
-
   return list;
 };
 
