f26f5937fff24b4deee9e1a42d9e080cb8176176
Patrick O'Shea <osheap81796@gmail.com>
2022-06-03 16:59:11 -0400

    Finished Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,35 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
+// I
+// O
+// C
+// E
 setPrototype.add = function(item) {
+  //Check if storage does not contain the item passed in
+  //Then add item to storage the key of the item and value true
+  if (!this.contains(item)) {
+    this._storage[item] = true;
+  }
 };
 
+// I : string
+// O : boolean
+// C :
+// E :
 setPrototype.contains = function(item) {
+  // if the key exists in the storage, return true
+  return this._storage[item] ? true : false;
 };
 
 setPrototype.remove = function(item) {
+  // delete the item from storage
+  delete this._storage[item];
 };
 
 /*
