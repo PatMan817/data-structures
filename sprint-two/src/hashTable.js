8a651c54c94a74d987a3c698b051fa87fc206b17
Patrick O'Shea <osheap81796@gmail.com>
2022-06-03 21:02:59 -0400

    Completed Hash Table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,39 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage[index] === undefined) {
+    this._storage[index] = [k, v];
+  } else {
+    this._storage[index].push([k, v]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage[index] !== undefined) {
+    if (this._storage[index].length > 1) {
+      for (var i = 0; i < this._storage[index].length; i++) {
+        if (this._storage[index][i][0] === k) {
+          return this._storage[index][i][1];
+        }
+      }
+    }
+    return this._storage[index][1];
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage[index] !== undefined) {
+    if (this._storage[index].length > 1) {
+      for (var i = 0; i < this._storage[index].length; i++) {
+        if (this._storage[index][i][0] === k) {
+          delete this._storage[index][i];
+        }
+      }
+    }
+    delete this._storage[index];
+  }
 };
 
 
