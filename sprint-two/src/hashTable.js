fc86e2de1f4957dfeea956b2809c04fab0a5c763
Patrick O'Shea <osheap81796@gmail.com>
2022-06-04 17:50:04 -0400

    Fixed Hash Table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,45 +7,50 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  if (this._storage[index] === undefined) {
-    this._storage[index] = [k, v];
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, [[k, v]]);
   } else {
-    this._storage[index].push([k, v]);
+    var innerArray = this._storage.get(index);
+    for (var i = 0; i < innerArray.length; i++) {
+      var isThere = false;
+      if (innerArray[i][0] === k) {
+        innerArray[i][1] = v;
+        isThere = true;
+      }
+    }
+    if (!isThere) {
+      innerArray.push([k, v]);
+    }
   }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  if (this._storage[index] !== undefined) {
-    if (this._storage[index].length > 1) {
-      for (var i = 0; i < this._storage[index].length; i++) {
-        if (this._storage[index][i][0] === k) {
-          return this._storage[index][i][1];
-        }
+  if (this._storage.get(index) !== undefined) {
+    for (var i = 0; i < this._storage.get(index).length; i++) {
+      if (this._storage.get(index)[i][0] === k) {
+        return this._storage.get(index)[i][1];
       }
     }
-    return this._storage[index][1];
   }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  if (this._storage[index] !== undefined) {
-    if (this._storage[index].length > 1) {
-      for (var i = 0; i < this._storage[index].length; i++) {
-        if (this._storage[index][i][0] === k) {
-          delete this._storage[index][i];
+  if (this._storage.get(index) !== undefined) {
+    if (this._storage.get(index).length > 1) {
+      for (var i = 0; i < this._storage.get(index).length; i++) {
+        if (this._storage.get(index)[i][0] === k) {
+          delete this._storage.get(index)[i];
         }
       }
     }
-    delete this._storage[index];
+    this._storage.set(index, undefined);
   }
 };
-
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
 
